5db6c969f1e884bf5728dfa048d8084f0ca7178f
varunkainth <sgolu5910@gmail.com>
2024-08-23 01:17:12 +0530

    "Removed useNewUrlParser and useUnifiedTopology options from mongoose connection"

--- backend/src/index.js
+++ backend/src/index.js
@@ -35,10 +35,7 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT;
 console.log(process.env.MONGO_URI);
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("Mongodb connection error:", err));
 

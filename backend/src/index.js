75743bd02ae7d03dbc191e144aea73aef4cd0f43
varunkainth <sgolu5910@gmail.com>
2024-08-23 00:38:28 +0530

    Google and Github Routes are complete

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,56 +1,60 @@
-require('dotenv').config();
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
-const authRoutes = require('./routes/auth');
-const documentRoutes = require('./routes/document');
-const mongoose = require('mongoose');
-const http = require('http');
-const { Server } = require('socket.io');
-
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const authRoutes = require("./routes/auth");
+const documentRoutes = require("./routes/document");
+const mongoose = require("mongoose");
+const http = require("http");
+const { Server } = require("socket.io");
+const Firebase = require("./config/firebase");
+require("dotenv").config();
 const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: '*',
-    },
+  cors: {
+    origin: "*",
+  },
 });
 
-app.use(cors())
+app.use(cors());
 
-io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-    socket.on('document-update', (data)=>{
-        socket.broadcast.emit('document-update', data);
-    })
+  socket.on("document-update", (data) => {
+    socket.broadcast.emit("document-update", data);
+  });
 
-    socket.on('disconnect', ()=>{
-        console.log('User disconnected', socket.id);
-    })
-})
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+  });
+});
 
 const PORT = process.env.PORT;
-
-mongoose.connect(process.env.MONGO_URI,{
+console.log(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(()=>console.log('MongoDB connected'))
-.catch((err)=> console.error('Mongodb connection error:',err));
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("Mongodb connection error:", err));
 
 app.use(helmet());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/documents', documentRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/documents", documentRoutes);
 
-app.get('/', (req, res) => {
-    res.send("Hello, Data");
+app.get("/", (req, res) => {
+  res.send("Hello, Data");
 });
 
+Firebase()
+
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`);
-})
+  console.log(`server is running on port ${PORT}`);
+});

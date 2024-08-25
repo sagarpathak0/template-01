fab2e99b7bb1f0554a0475c7868059b2288600f8
varunkainth <sgolu5910@gmail.com>
2024-08-25 12:48:57 +0530

    "Updated CORS origin and methods in backend, changed JWT token expiration, and modified frontend components and hooks for project data"

--- xcali/components/FeedPage.tsx
+++ xcali/components/FeedPage.tsx
@@ -26,16 +26,16 @@ const FeedPage: React.FC = () => {
     if (error) return <p className="text-center text-red-600">{error}</p>;
 
     return (
-        <div className="flex">
-            <div className="flex-1 mx-auto p-4">
+        <div className="flex min-h-screen">
+            <div className="flex-1 mx-auto overflow-auto p-4">
                 <h1 className="text-2xl font-bold mb-4">Feed</h1>
                 <div>
                     {projects.length > 0 ? (
                         projects.map((project) => (
                             <div key={project._id} className="mb-4 bg-white shadow rounded-lg p-4">
                                 <FeedItem
-                                    username={project.owner.username} // Assuming project has an owner with username
-                                    avatar={project.owner.avatar}     // Assuming project has an owner with avatar
+                                    username={project.username} 
+                                    avatar={project.avatar}     // Assuming project has an owner with avatar
                                     time={project.createdAt}          // Assuming project has createdAt timestamp
                                     content={project.description}     // Assuming project has a description
                                 />

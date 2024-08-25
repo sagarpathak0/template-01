8d276f1952ecb8e81497f067ec3e27232f602ad3
sagarpathak0 <sagar200422@gmail.com>
2024-08-25 11:19:19 +0530

    vre

--- xcali/pages/index.tsx
+++ xcali/pages/index.tsx
@@ -7,10 +7,11 @@ import { useAuth } from "@/hooks/useAuth";
 const Home: React.FC = () => {
   const { user, logout, isAuthenticated } = useAuth();
   return (
-    <main className="flex flex-col w-4/5 bg-gradient-to-r from-blue-900 via-purple-900 to-blue-900 text-white">
+    <main className="flex flex-col bg-gradient-to-r from-blue-900 via-purple-900 to-blue-900 text-white">
       {user?(
         <>
-          <FeedPage />
+          <div className="h-4/5"><FeedPage /></div>
+          
         </>
       ):(
         <>

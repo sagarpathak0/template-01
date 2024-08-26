daace7576677c56cc2d6e716453a19d1cb74f63d
sagarpathak0 <sagar200422@gmail.com>
2024-08-26 12:07:22 +0530

    rerer

--- xcali/components/FileUpload.tsx
+++ xcali/components/FileUpload.tsx
@@ -125,6 +125,7 @@ const FileUpload: React.FC<FileUploadProps> = () => {
                 Project Name
               </label>
               <input
+                placeholder="project-name"
                 type="text"
                 value={projectName}
                 onChange={(e) => setProjectName(e.target.value)}
@@ -138,6 +139,7 @@ const FileUpload: React.FC<FileUploadProps> = () => {
                 Project Description
               </label>
               <textarea
+                placeholder="description"
                 value={projectDescription}
                 onChange={(e) => setProjectDescription(e.target.value)}
                 className="block w-full mt-1 px-3 py-2 text-black border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
@@ -165,6 +167,7 @@ const FileUpload: React.FC<FileUploadProps> = () => {
                 Upload Files/Folders
               </label>
               <input
+                placeholder="upload"
                 type="file"
                 onChange={handleFileChange}
                 className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-600 hover:file:bg-indigo-100"
@@ -188,6 +191,7 @@ const FileUpload: React.FC<FileUploadProps> = () => {
                 Upload Thumbnail/Video
               </label>
               <input
+                placeholder="thumbnail"
                 type="file"
                 onChange={handleThumbnailChange}
                 className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded-md file:border-0 file:text-sm file:font-semibold file:bg-indigo-50 file:text-indigo-600 hover:file:bg-indigo-100"
